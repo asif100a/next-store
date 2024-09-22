d51200029ae7b3265c98fb6d67c0fde67cb89a88
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-09-23 00:38:56 +0600

    Active link successfully highlighted

--- src/app/components/Layout/Header.tsx
+++ src/app/components/Layout/Header.tsx
@@ -1,7 +1,9 @@
+"use client";
 import Image from 'next/image';
 import React from 'react';
 import nextStoreLogoLight from '@/app/assets/NextStore-light.png';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface NavButton {
     btnName: string,
@@ -14,6 +16,9 @@ interface NavLinks {
 };
 
 const Header: React.FC = () => {
+    const pathName = usePathname();
+    console.log(pathName);
+
     const navBtn: NavButton[] = [
         { btnName: 'Login', path: '/login' },
         { btnName: 'Sign Up', path: '/sign-up' },
@@ -43,20 +48,23 @@ const Header: React.FC = () => {
                         type="text"
                         name="SearchProduct"
                         id="SearchProduct"
+                        placeholder='Search your favourite product here'
                         className='border border-r-0 border-[#dd37fc] focus:outline-none bg-inherit text-[#6593fc] font-bold rounded-l-full w-[362px] px-6 py-3'
                     />
-                    <input 
-                    type="submit"
-                    value={'Search'}
-                    className='border border-l-0 border-[#dd37fc] focus:outline-none bg-[#dd37fc] text-[#161616] font-bold hover:cursor-pointer px-6 rounded-r-full py-3'
-                     />
+                    <input
+                        type="submit"
+                        value={'Search'}
+                        className='border border-l-0 border-[#dd37fc] focus:outline-none bg-[#dd37fc] text-[#161616] font-bold hover:cursor-pointer px-6 rounded-r-full py-3'
+                    />
                 </div>
 
                 {/* NavLinks */}
                 <ul className='flex gap-6'>
                     {
                         links.map((link, n) => (
-                            <li key={n}><Link href={link.path}>{link.linkName}</Link></li>
+                            <li key={n}>
+                                <Link href={link.path} className={pathName === link?.path ? 'text-[#6593fc]' : 'text-black'}>{link.linkName}</Link>
+                            </li>
                         ))
                     }
                 </ul>
@@ -64,7 +72,9 @@ const Header: React.FC = () => {
                 <div className='flex gap-6'>
                     {
                         navBtn.map((btn, n) => (
-                            <button key={n}><Link href={btn.path}>{btn.btnName}</Link></button>
+                            <button key={n}>
+                                <Link href={btn.path} className={pathName === btn?.path ? 'text-[#6593fc]' : 'text-black'}>{btn.btnName}</Link>
+                            </button>
                         ))
                     }
                 </div>
