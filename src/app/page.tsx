ba34c530114db42c10ac672b08910b809bdfe629
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-09-29 23:32:35 +0600

    Awesome navbar style added for- Mobile device

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ForYou from "./components/Sections/ForYou";
 
 export default function Home() {
   return (
-    <main className="w-auto lg:w-[996px] xl:w-[1248px] mx-auto py-4 md:py-6 min-h-[calc(100vh-445.9px)]">
+    <main className="w-auto lg:w-[996px] xl:w-[1248px] mx-auto py-3 md:py-6 min-h-[calc(100vh-445.9px)]">
       <ForYou />
       <Categories />
     </main>
