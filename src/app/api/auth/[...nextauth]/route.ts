41fdf91541eb465d7956a8f657aa07b6f66c72e0
N.M. Asif Sheikh <asifsheikhrrr@gmail.com>
2024-10-30 22:17:09 +0600

    Email & Password authentication added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,69 @@
 import NextAuth, { NextAuthOptions, User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
+// Extend the user interface to include the role
+declare module "next-auth" {
+    interface Session {
+        user: {
+            name?: string | null;
+            email?: string | null;
+            image?: string | null;
+            role?: string;
+        };
+    }
+
+    interface User {
+        role?: string;
+    }
+
+};
+
+// Extend the jwt to include the role
+declare module "next-auth/jwt" {
+    interface JWT {
+        role?: string;
+    }
+}
+
 export const authOptions: NextAuthOptions = {
     secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
     session: {
         strategy: 'jwt',
+        maxAge: 20 * 24 * 60 * 60
     },
     providers: [
         CredentialsProvider({
             credentials: {
                 email: { label: 'Email', type: 'email', required: true, placeholder: 'Your email address' },
                 password: { label: 'Password', type: 'password', required: true, placeholder: 'Your password' },
+                image: { label: 'Image', type: 'text', required: true, placeholder: 'Your image url' },
             },
             async authorize(credentials) {
-                if(!credentials) {
+                if (!credentials) {
                     return null;
                 }
                 else {
-                    const user: User = {id: '1', name: 'user', email: credentials?.email};
+                    const user: User = { id: '1', name: 'user', role: 'admin', email: credentials?.email, image: credentials?.image };
                     return user;
                 }
             }
         })
     ],
+    callbacks: {
+        async jwt({ token, user, account }) {
+            if (account) {
+                token.role = user.role;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            // If session.user exits, add the role
+            if (session.user) {
+                session.user.role = token.role;
+            }
+            return session;
+        },
+    }
 };
 
 const handler = NextAuth(authOptions);
--- src/app/my-lists/page.tsx
+++ src/app/my-lists/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import React from 'react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/route';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
     title: 'My Lists',
@@ -15,6 +16,11 @@ const page: React.FC = async() => {
     return (
         <section className='w-auto lg:w-[996px] xl:w-[1248px] mx-auto py-6 min-h-[calc(100vh-445.9px)]'>
             <h1>This is my list page</h1>
+            <div>
+                <h4>Name: {session?.user?.name}</h4>
+                <p><small>Email: {session?.user?.email}</small></p>
+                <Image src={session?.user?.image} alt={session?.user?.name} height={100} width={100} />
+            </div>
         </section>
     );
 };
